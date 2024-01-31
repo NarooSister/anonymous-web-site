d6c2073d4788ae82f39017ad48e2ce71b8829536
NarooSister <dongeun49@naver.com>
2024-01-31 21:46:32 +0900

    Initial commit

---
+++ src/main/java/com/example/anonymouswebsite/AnonymousWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.anonymouswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonymousWebSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/anonymouswebsite/AnonymousWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anonymouswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
